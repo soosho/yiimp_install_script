dbfddd68940ded4985edbd402279f13b1369c925
Soosho <162333149+soosho@users.noreply.github.com>
2025-03-12 10:07:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1029,113 +1029,155 @@
 
     # Make config file
     echo '
-    <?php
-    ini_set('"'"'date.timezone'"'"', '"'"'CET'"'"');
-    define('"'"'YAAMP_LOGS'"'"', '"'"'/var/log/yiimp'"'"');
-    define('"'"'YAAMP_HTDOCS'"'"', '"'"'/var/web'"'"');
+<?php
 
-    define('"'"'YAAMP_BIN'"'"', '"'"'/var/bin'"'"');
+ini_set("date.timezone", "UTC");
 
-    define('"'"'YAAMP_DBHOST'"'"', '"'"'localhost'"'"');
-    define('"'"'YAAMP_DBNAME'"'"', '"'"'yiimpfrontend'"'"');
-    define('"'"'YAAMP_DBUSER'"'"', '"'"'panel'"'"');
-    define('"'"'YAAMP_DBPASSWORD'"'"', '"'"''"${password}"''"'"');
+define("YAAMP_LOGS", "/var/www/log");
+define("YAAMP_HTDOCS", "/var/www");
+define("YAAMP_BIN", "/var/www/bin");
 
-    define('"'"'YAAMP_PRODUCTION'"'"', true);
-    define('"'"'YAAMP_RENTAL'"'"', false);
+define("YAAMP_DBHOST", "localhost");
+define("YAAMP_DBNAME", "yaamp");
+define("YAAMP_DBUSER", "root");
+define("YAAMP_DBPASSWORD", "password");
 
-    define('"'"'YAAMP_LIMIT_ESTIMATE'"'"', false);
+define("YAAMP_SITE_URL", "yiimp.ccminer.org");
+define("YAAMP_STRATUM_URL", YAAMP_SITE_URL); // change if your stratum server is on a different host
+define("YAAMP_SITE_NAME", "YiiMP");
 
-    define('"'"'YAAMP_FEES_SOLO'"'"', 0.5);
+define("YAAMP_PRODUCTION", true);
 
-    define('"'"'YAAMP_FEES_MINING'"'"', 0.5);
-    define('"'"'YAAMP_FEES_EXCHANGE'"'"', 2);
-    define('"'"'YAAMP_FEES_RENTING'"'"', 2);
-    define('"'"'YAAMP_TXFEE_RENTING_WD'"'"', 0.002);
+define("YIIMP_PUBLIC_EXPLORER", true);
+define("YIIMP_PUBLIC_BENCHMARK", false);
 
-    define('"'"'YAAMP_PAYMENTS_FREQ'"'"', 2*60*60);
-    define('"'"'YAAMP_PAYMENTS_MINI'"'"', 0.001);
+define("YAAMP_RENTAL", true);
+define("YAAMP_LIMIT_ESTIMATE", false);
 
-    define('"'"'YAAMP_ALLOW_EXCHANGE'"'"', false);
-    define('"'"'YIIMP_PUBLIC_EXPLORER'"'"', true);
-    define('"'"'YIIMP_PUBLIC_BENCHMARK'"'"', false);
+define("YAAMP_FEES_SOLO", 1);
+define("YAAMP_FEES_MINING", 0.5);
+define("YAAMP_FEES_EXCHANGE", 2);
+define("YAAMP_FEES_RENTING", 2);
+define("YAAMP_TXFEE_RENTING_WD", 0.002);
+define("YAAMP_PAYMENTS_FREQ", 3 * 60 * 60);
+define("YAAMP_PAYMENTS_MINI", 0.001);
 
-    define('"'"'YIIMP_FIAT_ALTERNATIVE'"'"', '"'"'USD'"'"'); // USD is main
-    define('"'"'YAAMP_USE_NICEHASH_API'"'"', false);
+define("YAAMP_ALLOW_EXCHANGE", false);
+define("YIIMP_FIAT_ALTERNATIVE", "EUR"); // USD is main
 
-    define('"'"'YAAMP_BTCADDRESS'"'"', '"'"'1C1hnjk3WhuAvUN6Ny6LTxPD3rwSZwapW7'"'"');
+define("YAAMP_USE_NICEHASH_API", false);
 
-    define('"'"'YAAMP_SITE_URL'"'"', '"'"''"${server_name}"''"'"');
-    define('"'"'YAAMP_STRATUM_URL'"'"', YAAMP_SITE_URL); // change if your stratum server is on a different host
-    define('"'"'YAAMP_SITE_NAME'"'"', '"'"'YIIMP'"'"');
-    define('"'"'YAAMP_ADMIN_EMAIL'"'"', '"'"''"${EMAIL}"''"'"');
-    define('"'"'YAAMP_ADMIN_IP'"'"', '"'"''"${Public}"''"'"'); // samples: "80.236.118.26,90.234.221.11" or "10.0.0.1/8"
+define("YAAMP_BTCADDRESS", "1Auhps1mHZQpoX4mCcVL8odU81VakZQ6dR");
 
-    define('"'"'YAAMP_ADMIN_WEBCONSOLE'"'"', true);
-    define('"'"'YAAMP_CREATE_NEW_COINS'"'"', false);
-    define('"'"'YAAMP_NOTIFY_NEW_COINS'"'"', false);
+define("YIIMP_ADMIN_LOGIN", false);
+define("YAAMP_ADMIN_EMAIL", "yiimp@spam.la");
+define("YAAMP_ADMIN_USER", "yiimpadmin");
+define("YAAMP_ADMIN_PASS", "set-a-password");
+define("YAAMP_ADMIN_IP", ""); // samples: "80.236.118.26,90.234.221.11" or "10.0.0.1/8"
+define("YAAMP_ADMIN_WEBCONSOLE", true);
+define("YAAMP_CREATE_NEW_COINS", true);
+define("YAAMP_NOTIFY_NEW_COINS", false);
+define("YAAMP_DEFAULT_ALGO", "x11");
 
-    define('"'"'YAAMP_DEFAULT_ALGO'"'"', '"'"'all'"'"');
+/* Github access token used to scan coin repos for new releases */
+define("GITHUB_ACCESSTOKEN", "<username>:<api-secret>");
 
-    define('"'"'YAAMP_USE_NGINX'"'"', true);
+/* mail server access data to send mails using external mailserver */
+define("SMTP_HOST", "mail.example.com");
+define("SMTP_PORT", 25);
+define("SMTP_USEAUTH", true);
+define("SMTP_USERNAME", "mailuser");
+define("SMTP_PASSWORD", "mailpassword");
+define("SMTP_DEFAULT_FROM", "mailuser@example.com");
+define("SMTP_DEFAULT_HELO", "mypool-server.example.com");
 
-    // Exchange public keys (private keys are in a separate config file)
-    define('"'"'EXCH_CRYPTOPIA_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_POLONIEX_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_BITTREX_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_BLEUTRADE_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_BTER_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_YOBIT_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_CCEX_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_COINMARKETS_USER'"'"', '"'"''"'"');
-    define('"'"'EXCH_COINMARKETS_PIN'"'"', '"'"''"'"');
-    define('"'"'EXCH_BITSTAMP_ID'"'"','"'"''"'"');
-    define('"'"'EXCH_BITSTAMP_KEY'"'"','"'"''"'"');
-    define('"'"'EXCH_HITBTC_KEY'"'"','"'"''"'"');
-    define('"'"'EXCH_KRAKEN_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_LIVECOIN_KEY'"'"', '"'"''"'"');
-    define('"'"'EXCH_NOVA_KEY'"'"', '"'"''"'"');
+define("YAAMP_USE_NGINX", false);
 
-    // Automatic withdraw to Yaamp btc wallet if btc balance > 0.3
-    define('"'"'EXCH_AUTO_WITHDRAW'"'"', 0.3);
+/* Sample config file to put in /etc/yiimp/keys.php */
 
-    // nicehash keys deposit account & amount to deposit at a time
-    define('"'"'NICEHASH_API_KEY'"'"','"'"'f96c65a7-3d2f-4f3a-815c-cacf00674396'"'"');
-    define('"'"'NICEHASH_API_ID'"'"','"'"'825979'"'"');
-    define('"'"'NICEHASH_DEPOSIT'"'"','"'"'3ABoqBjeorjzbyHmGMppM62YLssUgJhtuf'"'"');
-    define('"'"'NICEHASH_DEPOSIT_AMOUNT'"'"','"'"'0.01'"'"');
+define("YIIMP_MYSQLDUMP_USER", "root");
+define("YIIMP_MYSQLDUMP_PASS", "<my_mysql_password>");
 
-    $cold_wallet_table = array(
-	'"'"'1PqjApUdjwU9k4v1RDWf6XveARyEXaiGUz'"'"' => 0.10,
-    );
+/* 
+ * Exchange access keys
+ * for public fronted use separate container instance and leave keys unconfigured
+ *
+ * access tokens required to create/cancel orders and access your balances/deposit addresses
+ */
+define("EXCH_BINANCE_KEY", "");
+define("EXCH_BINANCE_SECRET", "");
 
-    // Sample fixed pool fees
-    $configFixedPoolFees = array(
-        '"'"'zr5'"'"' => 2.0,
-        '"'"'scrypt'"'"' => 20.0,
-        '"'"'sha256'"'"' => 5.0,
-     );
+define("EXCH_CEXIO_SECRET", "");
 
-     // Sample fixed pool fees solo
-    $configFixedPoolFeesSolo = array(
-        '"'"'zr5'"'"' => 2.0,
-        '"'"'scrypt'"'"' => 20.0,
-        '"'"'sha256'"'"' => 5.0,
+define("EXCH_EXBITRON_KEY", "");
 
-    );
+define("EXCH_HITBTC_SECRET", "");
+define("EXCH_HITBTC_KEY", "");
 
-    // Sample custom stratum ports
-    $configCustomPorts = array(
-    //	'"'"'x11'"'"' => 7000,
-    );
+define("EXCH_KRAKEN_KEY", "");
+define("EXCH_KRAKEN_SECRET", "");
 
-    // mBTC Coefs per algo (default is 1.0)
-    $configAlgoNormCoef = array(
-    //	'"'"'x11'"'"' => 5.0,
-    );
-    ' | sudo -E tee /var/web/serverconfig.php >/dev/null 2>&1
+define("EXCH_KUCOIN_SECRET", "");
 
-    echo -e "$GREEN Done...$COL_RESET"
+define("EXCH_POLONIEX_KEY", "");
+define("EXCH_POLONIEX_SECRET", "");
+
+define("EXCH_SAFETRADE_KEY", "");
+define("EXCH_SAFETRADE_SECRET", "");
+
+define("EXCH_TRADEOGRE_KEY", "");
+define("EXCH_TRADEOGRE_SECRET", "");
+
+define("EXCH_YOBIT_KEY", "");
+define("EXCH_YOBIT_SECRET", "");
+
+define("EXCH_NONKYC_KEY", "");
+define("EXCH_NONKYC_SECRET", "");
+
+define("EXCH_XEGGEX_KEY", "");
+define("EXCH_XEGGEX_SECRET", "");
+
+// Automatic withdraw to Yaamp btc wallet if btc balance > 0.3
+define("EXCH_AUTO_WITHDRAW", 0.3);
+
+// nicehash keys deposit account & amount to deposit at a time
+define("NICEHASH_API_KEY", "521c254d-8cc7-4319-83d2-ac6c604b5b49");
+define("NICEHASH_API_ID", "9205");
+define("NICEHASH_DEPOSIT", "3J9tapPoFCtouAZH7Th8HAPsD8aoykEHzk");
+define("NICEHASH_DEPOSIT_AMOUNT", "0.01");
+
+
+$cold_wallet_table = array(
+    "1C23KmLeCaQSLLyKVykHEUse1R7jRDv9j9" => 0.10,
+);
+
+// Sample fixed pool fees
+$configFixedPoolFees = array(
+    "zr5" => 2.0,
+    "scrypt" => 20.0,
+    "sha256" => 5.0,
+);
+
+// Sample fixed pool fees solo
+$configFixedPoolFeesSolo = array(
+    "zr5" => 2.0,
+    "scrypt" => 2.0,
+    "sha256" => 5.0,
+);
+
+// Sample custom stratum ports
+$configCustomPorts = array(
+//    "x11" => 7000,
+);
+
+// mBTC Coefs per algo (default is 1.0)
+$configAlgoNormCoef = array(
+//    "x11" => 5.0,
+);
+' | sudo -E tee /var/www/serverconfig.php >/dev/null 2>&1
+
+echo -e "$GREEN Done...$COL_RESET"
+
 
 
     # Updating stratum config files with database connection info
