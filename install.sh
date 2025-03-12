d34fed8ab2960d19d66af5af6ed716377e7cad8f
Soosho <162333149+soosho@users.noreply.github.com>
2025-03-12 10:12:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -991,33 +991,26 @@
     cd ~
     cd yiimp/sql
 
-    # Import sql dump
-    sudo zcat 2020-11-10-yaamp.sql.gz | sudo mysql --defaults-group-suffix=host1
+    # Import the initial sql dump
+    sudo zcat 2024-03-06-complete_export.sql.gz | sudo mysql --defaults-group-suffix=host1
 
-    # Oh the humanity!
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-04-24-market_history.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-04-27-settings.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-05-11-coins.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-05-15-benchmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-05-23-bookmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-06-01-notifications.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-06-04-bench_chips.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2016-11-23-coins.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2017-02-05-benchmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2017-03-31-earnings_index.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2020-06-03-blocks.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2017-05-accounts_case_swaptime.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2017-06-payouts_coinid_memo.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2017-09-notifications.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2017-10-bookmarks.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2018-09-22-workers.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2017-11-segwit.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2018-01-stratums_ports.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2018-02-coins_getinfo.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2019-03-coins_thepool_life.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2022-10-14-shares_solo.sql
-    sudo mysql --defaults-group-suffix=host1 --force < 2022-10-29-blocks_effort.sql
-    echo -e "$GREEN Done...$COL_RESET"
+    # Import other sql files in order
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-03-18-add_aurum_algo.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-03-29-add_github_version.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-03-31-add_payout_threshold.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-04-01-add_auto_exchange.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-04-01-shares_blocknumber.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-04-05-algos_port_color.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-04-22-add_equihash_algos.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-04-23-add_pers_string.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-04-29-add_sellthreshold.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2024-05-04-add_neoscrypt_xaya_algo.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2025-02-06-add_usemweb.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2025-02-13-add_xelisv2-pepew.sql
+    sudo mysql --defaults-group-suffix=host1 --force < 2025-02-23-add_algo_kawpow.sql
+
+echo "SQL imports completed successfully!"
+
 
 
     # Generating a basic Yiimp serverconfig.php
